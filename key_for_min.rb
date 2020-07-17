1a8989867c472814d46c27c996e9466b1be22524
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-17 02:33:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,4 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash) # This also works for this lab.
-  name_hash.sort do |x,y|
-     x <=> y
-  end.first[0]
-end
+key = hash.sort{|a,b| a[1] <=> b[1]}.first[0]
