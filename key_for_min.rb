8503bd86da7576440875b19469c43de7d054e617
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-17 02:33:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def reverse_sort_hash_value(hash)
+def key_for_min_value(hash)
    hash = hash.sort_by {|k,v| v}.reverse
 end
