b9d853a91b0052335bcc462912f199b0d890afaa
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-17 02:39:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  key = hash.sort{|a,b| a[1] <=> b[1]}.first
-  puts key
+  hash.min_by{|k,v| v}
 end
-
-
-hash = {"n" => 100, "m" => 100, "y" => 300, "d" => 200, "a" => 0, "z" => 300}          
-max = hash.values.max
-output_hash = Hash[hash.select { |k, v| v == max}]
-puts "#{output_hash.keys}"
