43e9b6c606b6f995c0323be856de1b380cae2d12
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-17 02:43:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,4 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-hash.each { |k, v| puts k if v == hash.values.max }
+def key_for_min_value(hash)
+  hash.each { |k, v| puts k if v == hash.values.min }
+end
